81cbac7f288878cebcb54d6770008316ac2c2a74
Luis Pirir <luispirir@gmail.com>
2014-03-21 10:46:30 -0600

    Add the module to patch the insert_record issue

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -28,3 +28,18 @@ ActiveSupport::Inflector.inflections do |inflect|
   inflect.irregular 'idioma', 'idiomas'
   inflect.uncountable %w( portal solicitud_informacion importar)
 end
+
+module ActiveRecord
+  module Associations
+    class AssociationProxy
+      def send(method, *args)
+        if proxy_respond_to?(method,true)
+          super
+        else
+          load_target
+          @target.send(method, *args)
+        end
+      end
+    end
+  end
+end
