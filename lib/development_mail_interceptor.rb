1a437366a034f238ae8eb2617610117dee998d8e
Javier Alvarez <jalvarezsamayoa@gmail.com>
2014-07-24 17:03:53 -0600

    Changed the email receptor

--- lib/development_mail_interceptor.rb
+++ lib/development_mail_interceptor.rb
@@ -1,6 +1,6 @@
 class DevelopmentMailInterceptor
   def self.delivering_email(message)
     message.subject = "#{message.to} #{message.subject}"
-    message.to = "luispirir@gmail.com"
+    message.to = "egob@transparencia.gob.gt"
   end
 end
